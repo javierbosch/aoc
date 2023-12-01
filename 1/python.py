bd496c2f17fa93e6b5d47b8816b58b94c6df2d40
Javier Bosch <javier.bosch@outlook.com>
2023-12-01 12:03:42 +0000

    day1 python

---
+++ 1/python.py
@@ -0,0 +1,3 @@
+import sys
+print('a = ', sum([ int("".join([[x for x in line if x.isdigit()][n] for n in (0,-1)])) for line in open(sys.argv[1], "r")]))
+print('b = ', sum([ int("".join([[x for x in line if x.isdigit()][n] for n in (0,-1)])) for line in ("".join(open(sys.argv[1], "r").readlines()).replace('one', 'one1one').replace('two', 'two2two').replace('three', 'three3three').replace('four', 'four4four').replace('five', 'five5five').replace('six', 'six6six').replace('seven', 'seven7seven').replace('eight', 'eight8eight').replace('nine', 'nine9nine').replace('zero', 'zero0zero').split('\n'))]))
